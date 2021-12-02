2e087492b4d566d207c5e85289d8ed4ba391e3f7
Leenawat Papahom <leenawat.pa@gmail.com>
2021-12-02 14:06:52 +0700

    test precommit

--- src/routes/example/index.ts
+++ src/routes/example/index.ts
@@ -2,6 +2,7 @@ import { FastifyPluginAsync } from 'fastify'
 
 const example: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
   fastify.get('/', async function (request, reply) {
+    console.log('hello world');
     return 'this is an example'
   })
 }
